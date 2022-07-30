d36cfbe4a1458b02d32e4c9ba02725fdb517bedb
Jonibek <jonibekqodirovv@gmail.com>
2022-07-30 13:35:08 +0300

    initial

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var elForms = document.querySelector(".js-form");
 var elName = document.querySelector(".js-input");
 var elMoney = document.querySelector(".js-input2");
-var elResults = document.querySelector(".js-result");
+var elResults = document.querySelector(".js-results");
 
 var checkMoney = 2000;
 
@@ -23,8 +23,8 @@ elForms.addEventListener("submit", function(e) {
 });
 
 
+// ----------------------------------------------------------- 2-VAZIFA -----------------------------------------------------------------------
 
-// 2-Vazifa
 
 
 var elForm = document.querySelector(".js-forms");
@@ -57,3 +57,40 @@ elForm.addEventListener("submit", function(e) {
         elResult.textContent = `Iltoms yoshingizni kiriting!!`;
     }
 });
+
+
+// ----------------------------------------------------------- 3-VAZIFA -----------------------------------------------------------------------
+
+
+// var elForm = document.querySelector(".js-forms");
+// var elAge = document.querySelector(".js-age");
+// var elResult = document.querySelector(".js-result");
+
+// var unknow = 0;
+// var minAge = 7;
+// var middleAge = 20;
+// var maxAge = 60;
+
+// elForm.addEventListener("submit", function(e) {
+//     e.preventDefault();
+
+//     var ageValue = elAge.value;
+
+//     if (ageValue > maxAge) {
+//         elResult.textContent = `Qariyalar uchun chipta bepul😊`;
+//     }
+//     else {
+//         if (ageValue >= unknow && ageValue < minAge) {
+//             elResult.textContent = `Sizga chipta bepul😊`;
+//         }
+//         else if (ageValue >= middleAge && ageValue < maxAge) {
+//             elResult.textContent = `Sizga chipta 1.600 so'm😃`;
+//         }
+//         else if (ageValue >= minAge && ageValue < middleAge) {
+//             elResult.textContent = `Sizga chipta 1.400 so'm`;
+//         }
+//         else {
+//             elResult.textContent = `Iltoms yoshingizni kiriting!!`;
+//         }
+//     }
+// });
